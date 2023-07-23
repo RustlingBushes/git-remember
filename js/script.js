2561157de9099be0b780113316ac4940664f4917
RustlingBushes <clack8926@gmail.com>
2023-07-23 12:23:06 +0300

    added user

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 let userName = 'Sergei';
 
 document.body.innerHTML = '<h1>Hello From sea side!</h1>';
+
+console.log('test2');
+
+const user = {
+	userName: 'Sergei',
+	age: 30,
+	work: false,
+};
